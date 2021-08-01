efc69142aa15dc5e12346557aa74ebfa1de943d2
Joshua <tantohyung@gmail.com>
2021-08-01 13:38:04 +0800

    bug fix on analyzer

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 // next.config.js
 const withPlugins = require("next-compose-plugins");
 const nextImages = require("next-images");
+
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+  enabled: true,
 });
 
-module.exports = withPlugins([
+const plugins = [
   [nextImages()],
   {
     images: {
       disableStaticImages: true,
     },
   },
-  [withBundleAnalyzer({})],
-]);
+];
+
+module.exports = process.env.ANALYZE
+  ? withPlugins([plugins, ...[[withBundleAnalyzer({})]]])
+  : withPlugins(plugins);
